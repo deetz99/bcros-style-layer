00c71a761afdf7a852ef28e655ce0bcb3832b3df
deetz99 <dietrich@wolpert.ca>
2024-01-22 12:22:20 -0800

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,10 @@ export default defineNuxtConfig({
   ui: {
     icons: ["mdi"],
   },
+  tailwindcss: {
+    configPath: join(currentDir, './tailwind.config.js'),
+    cssPath: join(currentDir, './assets/css/main.css'),
+  },
   alias: {
     tailwindFile: join(currentDir, "./tailwind.config.js"),
     BCGovFonts: join(currentDir, "./public/fonts/BCSans"),
